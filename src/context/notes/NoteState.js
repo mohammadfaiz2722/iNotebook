5e4f4362cef2c7f39f9dd736dae66018da4e444a
mohammadfaiz2722 <133015621+mohammadfaiz2722@users.noreply.github.com>
2024-09-07 16:18:44 +0530

    Updated

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -3,7 +3,7 @@ import NoteContext from "../NoteContext";
 import { useState} from "react";
 
 const NoteState = (props) => {
-  const host="http://localhost:5000"
+  const host="https://inotebook-12.onrender.com"
 // let noteInitial=[]
   const [notes,setNotes]=useState([])
   //Add a note
